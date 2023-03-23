90782ba8a53995d7cbcdf82c655b499d8d2403b3
forlived <1433973037@qq.com>
2023-03-23 21:18:31 +0800

    异步秒杀

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -12,12 +12,15 @@ import com.hmdp.utils.SimpleRedisLock;
 import com.hmdp.utils.UserHolder;
 import lombok.val;
 import org.springframework.aop.framework.AopContext;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
+import java.util.Collections;
 
 /**
  * <p>
@@ -32,37 +35,57 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
 
     @Resource
     private ISeckillVoucherService seckillVoucherService;
-    private RedisIdWorker redisIDWorker;
+    private RedisIdWorker redisIdWorker;
     private StringRedisTemplate stringRedisTemplate;
-
+    private static final DefaultRedisScript<Long> SECKILL_SCRIPT;
+    static {
+        SECKILL_SCRIPT=new DefaultRedisScript<>();
+        SECKILL_SCRIPT.setLocation(new ClassPathResource("seckill.lua"));
+        SECKILL_SCRIPT.setResultType(Long.class);
+    }
     @Override
     public Result seckillVoucher(Long voucherId) {
-        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
-        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
-            return Result.fail("未开始");
+        //异步秒杀
+        Long result = stringRedisTemplate.execute(SECKILL_SCRIPT,
+                Collections.emptyList(), voucherId.toString(),
+                UserHolder.getUser().getId().toString());
+        //2. 判断返回值，并返回错误信息
+        if (result.intValue() != 0) {
+            return Result.fail(result.intValue() == 1 ? "库存不足" : "不能重复下单");
         }
-        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
-            return Result.fail("已结束");
-        }
-        if (voucher.getStock()<1) {
-            return  Result.fail("库存不足");
-        }
-        Long userId= UserHolder.getUser().getId();
+        long orderId = redisIdWorker.nextId("order");
+        //TODO 保存阻塞队列
+
+        //3. 返回订单id
+        return Result.ok(orderId);
+
+
+//        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+//        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+//            return Result.fail("未开始");
+//        }
+//        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+//            return Result.fail("已结束");
+//        }
+//        if (voucher.getStock()<1) {
+//            return  Result.fail("库存不足");
+//        }
+//        Long userId= UserHolder.getUser().getId();
 //        synchronized(userId.toString().intern()) {
 //            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
 //            return proxy.createVoucherOrder(voucherId);
 //        }
         //创建锁对象
-        SimpleRedisLock lock = new SimpleRedisLock(stringRedisTemplate, "order:" + userId);
-        if (!lock.tryLock(5)) {
-             return Result.fail("重复下单");
-        }
-        try{
-            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
-            return proxy.createVoucherOrder(voucherId);
-        }finally {
-            lock.unlock();
-        }
+//        SimpleRedisLock lock = new SimpleRedisLock(stringRedisTemplate, "order:" + userId);
+//        if (!lock.tryLock(5)) {
+//             return Result.fail("重复下单");
+//        }
+//        try{
+//            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+//            return proxy.createVoucherOrder(voucherId);
+//        }finally {
+//            lock.unlock();
+//        }
 
     }
     @Override
@@ -84,7 +107,7 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         }
 
         VoucherOrder voucherOrder=new VoucherOrder();
-        long id=redisIDWorker.nextId("order");
+        long id=redisIdWorker.nextId("order");
         voucherOrder.setId(id);
 
         voucherOrder.setUserId(userId);
